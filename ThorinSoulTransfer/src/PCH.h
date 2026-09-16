ca0b03bddbd1337148cfe8d9d55f0db849bf9165
HDLan1202 <wxgv2vfxdk@privaterelay.appleid.com>
2026-09-16 02:42:26 +0100

    Add Thorin Soul Transfer precompiled header

---
+++ ThorinSoulTransfer/src/PCH.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <cctype>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+using namespace std::literals;
