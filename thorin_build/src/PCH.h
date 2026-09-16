aa1b6633cceff6edb0e428759dee626a1d798668
HDLan1202 <wxgv2vfxdk@privaterelay.appleid.com>
2026-09-16 12:44:02 +0100

    Add ThorinDragonSoulVisuals precompiled header

---
+++ thorin_build/src/PCH.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <string_view>
+#include <thread>
+
+using namespace std::literals;
