e008e0e06cabe6391a3af9031f83c30dd13647bd
HDLan1202 <wxgv2vfxdk@privaterelay.appleid.com>
2026-09-16 02:36:33 +0100

    build: add headers for Thorin visual plugin

--- src/PCH.h
+++ src/PCH.h
@@ -3,9 +3,24 @@
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
 
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <algorithm>
 #include <array>
-#include <cstddef>
+#include <atomic>
+#include <chrono>
+#include <cctype>
 #include <cstdint>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <ranges>
+#include <sstream>
+#include <string>
 #include <string_view>
+#include <vector>
 
 using namespace std::literals;
